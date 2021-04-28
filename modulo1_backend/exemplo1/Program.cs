c36fd3230dfc668d77308147b4c056d5718100ca
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-04-27 22:21:41 -0300

    initial commit

---
+++ modulo1_backend/exemplo1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace modulo1_backend
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+      byte number = 2;
+      int count = 0;
+      float price = 20.95f;
+      char letter = 'A';
+      string name = "Daniel";
+      bool isEmpty = false;
+
+      Console.WriteLine(number);
+      Console.WriteLine(count);
+      Console.WriteLine(price);
+      Console.WriteLine(letter);
+      Console.WriteLine(name);
+      Console.WriteLine(isEmpty);
+
+      var s = "345";
+      var r = Convert.ToInt32(s);
+      Console.WriteLine(r);
+
+    }
+  }
+}
---
+++ modulo1_backend/exemplo2Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exemplo2Classes
+{
+    public class Pessoa{
+        public byte idade;
+        public string name;
+
+        public void introducao(){
+            Console.WriteLine("Ola, meu nome é "+name+" e minha idade é "+idade+" anos.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pessoa = new Pessoa();
+            pessoa.name = "Daniel";
+            pessoa.idade = 23;
+            pessoa.introducao();
+        }
+    }
+}
